c69471def1e6ba0242875b13dca211589351ebda
C0C05 <alvaropucheg@gmail.com>
2022-11-22 14:20:54 +0100

    2º version de instafinder

--- Instafinder.sh
+++ Instafinder.sh
@@ -17,9 +17,17 @@ figlet INSTAFINDER
 echo -e "${blanco}Programa automatizado para la busqueda de nombres de usuario en Instagram \nGracias a ${magenta}OSRFramework ${blanco}y ${magenta}Osintgram"
 echo -e "${rojob}'Comprobar credenciales de Instagram si no funciona a la primera' $blanco \n"
 printf '%s%*s%s' "$(tput setaf 1)" 63 "==> C0C05_ <==" "$NORMAL"
+echo -e "\n"
 
 
+mostrarAyuda(){
+	echo -e  "$blanco usage: Instafinder [-n <NOMBRE>] [-a <1º APELLIDO>] [-x <2º APELLIDO>] [-c <CIUDAD>] [-p <PAIS>] [-y <AÑO>]
 
+  -h, --help            shows this help and exists.
+\n
+  Todos los campos son  necesarios, también se puede ejecutar el script sin ningún parametro.\n"
+	exit 0
+}
 genUsers(){
 	echo -en "\n\n$amarillo [?] $blanco Inserte el nombre: $magenta "
 	read nombre
@@ -34,11 +42,34 @@ genUsers(){
 	echo -en "\n\n$amarillo [?] $blanco Inserte pais: $magenta"
 	read pais
 
-	echo -e "\n\n$aqua 	GENERANDO NOMBRES DE USUARIO POSIBLES \n"
+	echo -e "\n\n$aqua	GENERANDO NOMBRES DE USUARIO POSIBLES \n"
 	sleep 0.2
 	alias_generator -n "$nombre" -s1 "$apellido" -s2 "$apellido2" -c "$ciudad" -C "$pais" -y "$year" -o $users  >/dev/null 2>/dev/null
 
 }
+
+genUsersParam(){
+	while getopts n:a:x:c:p:y: flag
+        do
+            case "${flag}" in
+                n) nombre=${OPTARG};;
+                a) apellido=${OPTARG};;
+                x) apellido2=${OPTARG};;
+                c) ciudad=${OPTARG};;
+                p) pais=${OPTARG};;
+                y) year=${OPTARG};;
+		*) mostrarAyuda;;
+            esac
+        done
+        echo -e "\n\n$aqua      GENERANDO NOMBRES DE USUARIO POSIBLES \n"
+        sleep 0.2
+        alias_generator -n "$nombre" -s1 "${apellido}" -s2 "$apellido2" -c "$ciudad" -C "$pais" -y "$year" -o $users >/dev/null 2>/dev/null
+	if ! [ -s $users ]
+	then
+		mostrarAyuda
+	fi
+}
+
 
 userFinder(){
 	echo -en "\n\n\n$amarillo [+] $blanco Comprobando nombres de usuarios validos \n\n"
@@ -51,6 +82,14 @@ userFinder(){
 	wait;
 	echo -e "\n\n$aqua      MOSTRANDO USUARIOS DISPONIBLES EN INSTAGRAM. ${rojo}ARCHIVO DE SALIDA ==> ${blanco}./validUsers \n"
 	sleep 2
+	if ! [ -s validUsers ]
+	then
+		echo -e "$rojo Comprobar credenciales, o el estado de conexión con la api de instagram $fin"
+		python3 ./Osintgram/main.py test -c exit >/dev/null 2>./errors.txt
+		cat ./errors.txt | tail -1
+		echo -e "\n"
+		exit 1
+	fi
 	cat validUsers | pr -3 -S"   " -T
 	echo -e "\n${rojo}chaoo ;)\n"
 	exit 0
@@ -64,8 +103,6 @@ comprobaciones(){
 	if [ ! -f ./validUsers ]
 	then
         	touch ./validUsers
-	else
-        	echo "" > ./validUsers
 	fi
 
 	which python3 >/dev/null
@@ -103,28 +140,25 @@ comprobaciones(){
 }
 
 login(){
-	echo -e "\n$aqua	AHORA INSERTAREMOS NUESTRAS CREDENCIALES DE INSTAGRAM PARA LA BUSQUEDA"
+	credFile="./config/credentials.ini"
+	echo -e "\n$aqua      AHORA INSERTAREMOS NUESTRAS CREDENCIALES DE INSTAGRAM PARA LA BUSQUEDA"
 	sleep 0.3
         echo -en "\n\n$amarillo [?] $blanco Nombre de usuario de instagram: $verde"
         read username
         echo -en "\n\n$amarillo [?] $blanco Contraseña de instagram: "
         read -s pass
-	grep -q "username = $username" $credFile
-        validUser=$?
-        grep -q "password = $pass" $credFile
-        validPass=$?
-
-        if [[ $validUser -eq 1 || $validPass -eq 1 ]]
-        then
                 echo -e "\n\n$amarillo 	[+] $blanco Validando usuario \n"
                 echo "[Credentials]
 username = $username
 password = $pass" > $credFile
 
-        fi
 }
 
-comprobaciones
-genUsers
+case $1 in
+	"") comprobaciones; genUsers ;;
+	"-h" | "--help") mostrarAyuda ;;
+	*) genUsersParam $@ ;;
+esac
+
 login
 userFinder
